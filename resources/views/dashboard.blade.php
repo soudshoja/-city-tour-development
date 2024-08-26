72a5b886979da1df35199e151f4fbbe3550545da
Tarek Barakat <tarekbarakat000@gmail.com>
2024-08-26 14:41:37 +0800

    mail auth

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Company's Dashboard') }}
+            {{ __("Company's Dashboard") }}
         </h2>
     </x-slot>
 

df253ed5af72d6f1b0c2ce1f5f63e249222665da
AmirSyafiq2112 <57993402+AmirSyafiq2112@users.noreply.github.com>
2024-11-08 16:57:17 +0800

    add branch role

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -9,7 +9,22 @@ class Branch extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'name',
+        'email',
+        'phone',
+        'address',
+        'company_id',
+    ];
 
-    protected $fillable = ['name', 'email', 'phone', 'address'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }

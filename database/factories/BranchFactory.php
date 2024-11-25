4b48a18669b2c832f6738a6b85b3c349934d0656
AmirSyafiq2112 <57993402+AmirSyafiq2112@users.noreply.github.com>
2024-11-25 18:06:22 +0800

    add notification testing

---
+++ database/factories/BranchFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Branch>
+ */
+class BranchFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => rand(1, 10),
+            'name' => $this->faker->company,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'company_id' => rand(1, 10),
+        ];
+    }
+}

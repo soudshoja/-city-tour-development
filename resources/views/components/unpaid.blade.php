9fd6f66af6d03f0f92f667b4378fe07e36314ac6
AmirSyafiq2112 <57993402+AmirSyafiq2112@users.noreply.github.com>
2024-11-28 18:18:45 +0800

    complete agent crm

--- resources/views/components/unpaid.blade.php
+++ resources/views/components/unpaid.blade.php
@@ -1,4 +1,4 @@
 
-<div {{ $attributes->merge(['class' => 'bg-red-500 text-white font-bold rounded-md shadow-md p-2 w-full']) }}>
+<div {{ $attributes->merge(['class' => 'bg-gradient-to-r from-red-500 to-red-700 text-white font-bold rounded-md shadow-md p-2 w-full']) }}>
     <p class="text-center">{{ $slot }}</p>
 </div>

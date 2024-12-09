53ab0b5aff0fd26c4dcc9d799ad2392a20a20998
AmirSyafiq2112 <amirabrashid@yahoo.com>
2024-12-09 18:22:16 +0800

    remove alpine init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
 </head>
 
 <body>
-    <!-- fix it ya nsooooom --> @include('layouts.alert')
+    <!-- fix it ya nsooooom --> 
+     @include('layouts.alert')
 
     <!-- Top Navigation -->
     <div>

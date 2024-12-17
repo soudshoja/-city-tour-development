ef662322a7a1bf4cbd786804b6afc0e8f98b0e05
AmirSyafiq2112 <amirabrashid@yahoo.com>
2024-12-17 21:40:58 +0800

    fix controller that need auth

--- routes/web.php
+++ routes/web.php
@@ -185,15 +185,18 @@ Route::group([
 });
 
 // INVOICE
-Route::get('/company/agents/invoices', [InvoiceController::class, 'companyAgentsInvoices'])->name('invoices.company.agents');
-Route::get('/invoice/create', [InvoiceController::class, 'create'])->name('invoice.create');
+Route::middleware('auth')->group(function () {
+    Route::get('/invoice/{id}', [InvoiceController::class, 'index'])->name('invoice.index');
+    Route::get('/sale-invoice', [InvoiceController::class, 'salelist'])->name('invoice.salelist');
+    Route::get('/invoice/create', [InvoiceController::class, 'create'])->name('invoice.create');
+    Route::get('/company/agents/invoices', [InvoiceController::class, 'companyAgentsInvoices'])->name('invoices.company.agents');
+});
+
 Route::get('/invoice/{invoiceNumber}', [InvoiceController::class, 'show'])->name('invoice.show');
 Route::post('/invoice/store', [InvoiceController::class, 'store'])->name('invoice.store');
-Route::get('/invoice/{id}', [InvoiceController::class, 'index'])->name('invoice.index');
 Route::put('/invoice/{id}', [InvoiceController::class, 'update'])->name('invoice.update');
 Route::patch('/invoices/{invoice}/status', [InvoiceController::class, 'updateStatus'])->name('invoices.updateStatus');
 Route::post('/invoices/clientadd', [InvoiceController::class, 'clientAdd'])->name('invoices.clientAdd');
-Route::get('/sale-invoice', [InvoiceController::class, 'salelist'])->name('invoice.salelist');
 Route::get('/invoice/edit/{invoiceNumber}', [InvoiceController::class, 'edit'])->name('invoice.edit');
            
 // PAYMENT

3e598a0bdea473d8574960eb9c509383d8fbe748
User <markrizam@gmail.com>
2025-02-03 16:09:07 +0800

    update api pending task

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -120,7 +120,12 @@ class MobileController extends Controller
 
     public function task()
     {
-        return response()->json(Task::all(), 200);
+        $tasks = Task::with('invoiceDetail')->get()->map(function ($task) {
+            $task->is_invoiced = $task->invoiceDetail()->exists();
+            return $task;
+        });
+    
+        return response()->json($tasks, 200);
     }
 
     public function getTasksByAgentId($agentId)

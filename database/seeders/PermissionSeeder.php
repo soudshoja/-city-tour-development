c00e631ce882b556beaeb8cb2ca3818fc23d3caa
AmirSyafiq2112 <amirabrashid@yahoo.com>
2025-02-04 17:56:04 +0800

    update responsive design and refactor navigation layout

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
+use App\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {

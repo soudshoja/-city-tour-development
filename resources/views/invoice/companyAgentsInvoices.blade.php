53e7f540a03d189feeb4ddcacf695250d3dcfb57
User <markrizam@gmail.com>
2025-02-20 11:31:11 +0800

    update invoice edit

--- resources/views/invoice/companyAgentsInvoices.blade.php
+++ resources/views/invoice/companyAgentsInvoices.blade.php
@@ -128,6 +128,7 @@
                                                 </g>
                                             </svg>
                                         </a>
+                                        @if ($invoice->status !== 'paid')
                                         <a href="{{ route('invoice.edit', ['invoiceNumber' => $invoice->invoice_number]) }}"
                                             class="text-sm font-medium text-blue-600 hover:underline">
 
@@ -135,6 +136,7 @@
                                                 <path fill="none" stroke="#00ab55" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m4.144 16.735l.493-3.425a.97.97 0 0 1 .293-.587l9.665-9.664a1.03 1.03 0 0 1 .973-.281a5.1 5.1 0 0 1 2.346 1.372a5.1 5.1 0 0 1 1.384 2.346a1.07 1.07 0 0 1-.282.973l-9.664 9.664a1.17 1.17 0 0 1-.598.294l-3.437.492a1.044 1.044 0 0 1-1.173-1.184m8.633-11.846l4.41 4.398M3.79 21.25h16.42" opacity=".5" />
                                             </svg>
                                         </a>
+                                        @endif
                                         <div id="viewInvoiceModal"
                                             class="fixed z-10 inset-0 flex items-center justify-center backdrop-blur-sm hidden">
                                             <div class="relative">
@@ -177,7 +179,6 @@
 
 
                     <!-- pagination -->
-                    @if ($invoices->count() > 15)
                     <div class="dataTable-bottom justify-center">
                         <nav class="dataTable-pagination">
                             <ul class="dataTable-pagination-list flex gap-2 mt-4">
@@ -201,7 +202,7 @@
                             </ul>
                         </nav>
                     </div>
-                    @endif
+
                     <!-- ./pagination -->
                 </div>
             </div>

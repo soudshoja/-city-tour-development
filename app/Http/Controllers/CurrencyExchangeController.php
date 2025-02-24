dcd010027effea3bdab568d90aeafa018c34d65f
AmirSyafiq2112 <amirabrashid@yahoo.com>
2025-02-24 17:51:02 +0800

    Add update method to CurrencyExchangeController for batch updating exchange rates

--- app/Http/Controllers/CurrencyExchangeController.php
+++ app/Http/Controllers/CurrencyExchangeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\CurrencyExchange;
+use Exception;
 use Illuminate\Http\Request;
 
 class CurrencyExchangeController extends Controller
@@ -13,4 +14,27 @@ class CurrencyExchangeController extends Controller
 
         return view('currency-exchange.index', compact('currencyExchanges'));
     }
+
+    public function update(Request $request){
+
+        $request->validate([
+            'id.*' => 'required',
+            'exchange_rate.*' => 'required',
+        ]);
+
+        foreach($request->all() as $exchange){
+            try {
+                $currencyExchange = CurrencyExchange::find($exchange['id']);
+                $currencyExchange->exchange_rate = $exchange['exchange_rate'];
+                $currencyExchange->updated_at = now();
+                $currencyExchange->save();
+            } catch (Exception $e) {
+                return response()->json(['message' => $e->getMessage()], 500);
+            }
+        }
+
+        return response()->json(['message' => 'Currency exchange rate updated successfully'], 200);
+    }
+
+    
 }

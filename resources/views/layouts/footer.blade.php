f45a560abf9344da83d11f08f304a9bf70d43ff1
azran-alphia-dev <azran@alphia.net>
2025-03-05 16:22:07 +0800

    Fixed footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,15 @@
 <!-- start footer section -->
 <!-- desktok footer section -->
 <div class="p-5 mt-5 text-center dark:text-[#f3f4f6] bg-white dark:bg-gray-900 rounded-md shadow-md">
-    © <span id="footer-year">2024</span> city tour.  <span id="footer-version">Version 1.0</span>
+    © <span id="footer-year">2024 - 2025</span> City Tour. <span id="footer-version">Version 1.0</span>
 </div>
 <!-- desktok footer section end-->
 <!-- Mobile footer section -->
 <div class="CityDisplaayNoneDesk">
-   
+
     <div class="mt-auto p-6 pt-0 text-center dark:text-white-dark ltr:sm:text-left rtl:sm:text-right">
         <!-- Mobile navigation bar -->
-        
+
 
 
 
@@ -21,11 +21,10 @@
 <!-- Scripts -->
 
 <script>
-
-fetch("{{ route('version.getCurrent') }}")
+    fetch("{{ route('version.getCurrent') }}")
         .then(response => response.json())
         .then(data => {
-            if (data && data.value) { 
+            if (data && data.value) {
                 // Assuming `value` holds the version, update the version dynamically
                 const versionElement = document.getElementById('footer-version');
                 if (versionElement) {
@@ -35,12 +34,12 @@ fetch("{{ route('version.getCurrent') }}")
         })
         .catch(error => console.error("Error fetching version:", error));
 
-// Check localStorage for the dark mode setting before the page is fully loaded
-if (localStorage.getItem('darkMode') === 'true') {
-    document.documentElement.classList.add('dark');
-} else {
-    document.documentElement.classList.remove('dark');
-}
+    // Check localStorage for the dark mode setting before the page is fully loaded
+    if (localStorage.getItem('darkMode') === 'true') {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
 </script>
 <!-- Include Chart.js from CDN -->
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>

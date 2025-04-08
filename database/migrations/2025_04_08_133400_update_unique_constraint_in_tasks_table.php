4c3ff7e21c417f0729a2b1ce6bc809ddd9168ffa
ShobakMoha <70998185+ShobakMoha@users.noreply.github.com>
2025-04-08 15:14:33 +0800

    update tasks table migration

--- database/migrations/2025_04_08_133400_update_unique_constraint_in_tasks_table.php
+++ database/migrations/2025_04_08_133400_update_unique_constraint_in_tasks_table.php
@@ -9,19 +9,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            
-            $table->dropUnique(['supplier_id', 'reference']); // Drop the existing unique constraint on supplier_id and reference
 
-            $table->unique(['supplier_id', 'reference', 'company_id']);
+            $table->unique(['supplier_id', 'reference', 'company_id'])->change();
         });
     }
 
     public function down(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropUnique(['supplier_id', 'reference', 'company_id']);
-
-            $table->unique(['supplier_id', 'reference']);
+            
+            $table->unique(['supplier_id', 'reference'])->change();
         });
     }
 };

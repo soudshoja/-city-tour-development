f9ca577ef805afa75f2a6d40bdcd5e2ef034fef9
MaizatulMokhtar <maizatulmokhtar85@gmail.com>
2025-06-20 16:34:37 +0800

    enhanced component

--- app/View/Components/SearchableDropdown.php
+++ app/View/Components/SearchableDropdown.php
@@ -12,23 +12,18 @@ class SearchableDropdown extends Component
     public $label;
     public $name;
     public $items;
-    public $selected;
     public $placeholder;
     public $maxResults;
 
-    public function __construct($label = '', $name = '', $items = '', $selected = '', $placeholder = '', $maxResults = '')
+    public function __construct($label = '', $name = '', $items = '', $placeholder = '', $maxResults = '')
     {
         $this->label = $label;
         $this->name = $name;
         $this->items = $items;     
-        $this->selected = $selected;
         $this->placeholder = $placeholder;
         $this->maxResults = $maxResults;
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         return view('components.searchable-dropdown');

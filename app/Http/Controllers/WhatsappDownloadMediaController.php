5182e68e716d21c49c883db291c0e684879ec942
AmirSyafiq2112 <amirabrashid@yahoo.com>
2025-06-24 15:18:00 +0800

    fix psr-4

--- app/Http/Controllers/WhatsappDownloadMediaController.php
+++ app/Http/Controllers/WhatsappDownloadMediaController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
 
-class DownloadMediaController extends Controller
+class WhatsappDownloadMediaController extends Controller
 {
     public function download(Request $request)
     {

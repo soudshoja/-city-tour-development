b45bac5702d2813214aa71aa60c628cda5aa0066
AmirSyafiq2112 <amirabrashid@yahoo.com>
2025-07-30 09:26:11 +0800

    initiate client agent

---
+++ database/migrations/2025_07_30_091205_create_agent_clients_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Models\Client;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('agent_clients', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('agent_id')->constrained('agents')->onDelete('cascade');
+            $table->foreignId('client_id')->constrained('clients')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        $clients = Client::all();
+
+        try{
+            foreach($clients as $client) {
+                $client->agents()->sync($client->agents->pluck('id')->toArray());
+                $client->save();
+            }
+        } catch (Exception $e) {
+            Log::error('Error syncing agents for clients: ' . $e->getMessage());
+        }
+
+        Schema::table('clients', function (Blueprint $table) {
+            $table->dropForeign(['agent_id']);
+            $table->dropColumn('agent_id');
+        });
+    }
+
+    public function down(): void
+    {
+        $clients = Client::all();
+
+        Schema::table('clients', function (Blueprint $table) {
+            $table->foreignId('agent_id')->nullable()->constrained('agents')->onDelete('set null');
+        });
+
+        foreach ($clients as $client){
+            $client->agent_id = $clients->agents->first()->id ?? null;
+            $client->save();
+        }
+
+        Schema::dropIfExists('agent_clients');
+    }
+};

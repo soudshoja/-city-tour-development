bf41c92666a80a1bd242d5c92abd0d951546d80d
ShobakMoha <70998185+ShobakMoha@users.noreply.github.com>
2025-08-02 15:51:35 +0800

    add visa and insurance columns to suppliers table

---
+++ database/migrations/2025_07_29_032121_add_visa_and_insurance_to_suppliers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->boolean('has_visa')->default(false)->after('has_flight');
+            $table->boolean('has_insurance')->default(false)->after('has_visa');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->dropColumn('has_visa');
+            $table->dropColumn('has_insurance');
+        });
+    }
+};

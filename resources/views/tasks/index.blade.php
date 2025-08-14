7f2ef2394e61d99539d0facbf8cea8b608817d20
nlyaizzati <alya@alphia.net>
2025-08-14 16:21:13 +0800

    update validation rules to include Smile Holidays as a merge supplier

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2075,7 +2075,7 @@
                     'border-gray-300', 'dark:border-gray-700', 'dark:bg-gray-800',
                     'dark:text-gray-300', 'p-3', 'mb-1');
                 formTaskContainer.appendChild(input);
-            } else if (supplier.name == 'TBO Car' || supplier.name == 'TBO Air') {
+            } else if (supplier.name == 'TBO Car' || supplier.name == 'TBO Air' || supplier.name == 'Smile Holidays') {
                 const batches = [];
                 let active = 0;
 

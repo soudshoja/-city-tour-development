5b5cc9e8a81506de794f5c23143633c0c1092c8a
AmirSyafiq2112 <amirabrashid@yahoo.com>
2025-08-21 17:16:57 +0800

    refactor: streamline supplier retrieval logic in index method

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -104,6 +104,7 @@ class TaskController extends Controller
             });
         }
         $countries = Country::all();
+        $suppliers = Supplier::with('companies');
 
         if ($user->role_id == Role::ADMIN) {
             $tasks = $tasks;
@@ -118,25 +119,24 @@ class TaskController extends Controller
             $clients = Client::whereIn('agent_id', $agentsId)->get();
             $tasks = $tasks->where('company_id', $user->company->id);
 
-            $suppliers = Supplier::whereHas('companies', function ($query) use ($user) {
-                $query->where('company_id', $user->company->id)->where('is_active', true);
-            })->get();
+            // $suppliers = Supplier::whereHas('companies', function ($query) use ($user) {
+            //     $query->where('company_id', $user->company->id)->where('is_active', true);
+            // })->get();
 
-            // Add is_active property for each supplier based on pivot 'active' field
-            $suppliers->transform(function ($supplier) use ($user) {
-                $company = $supplier->companies()->where('company_id', $user->company->id)->first();
-                $supplier->is_active = $company && isset($company->pivot->is_active) ? (bool)$company->pivot->is_active : false;
-                return $supplier;
+            $suppliers = $suppliers->whereHas('companies', function ($query) use ($user) {
+                $query->where('company_id', $user->company->id); 
             });
+
         } elseif ($user->role_id == Role::BRANCH) {
             $agents = Agent::with('branch')->where('branch_id', $user->branch_id)->get();
             $agentsId = $agents->pluck('id');
             $clients = Client::whereIn('agent_id', $agentsId)->get();
             $tasks = $tasks->whereIn('agent_id', $agentsId)->where('company_id', $user->company_id);
 
-            $suppliers = Supplier::whereHas('companies', function ($query) use ($user) {
-                $query->where('company_id', $user->branch->company_id);
-            })->get();
+            $suppliers = $suppliers->whereHas('companies', function ($query) use ($user) {
+                $query->where('company_id', $user->company_id);
+            });
+
         } elseif ($user->role_id == Role::AGENT) {
 
             $agents = Agent::with('branch')->where('id', $user->agent->id)->get();
@@ -152,13 +152,18 @@ class TaskController extends Controller
             })->where('company_id', $user->agent->branch->company_id);
 
             $companyId = $user->agent->branch->company_id;
-            $suppliers = Supplier::whereHas('companies', function ($query) use ($companyId) {
+            $suppliers = $suppliers->whereHas('companies', function ($query) use ($companyId) {
                 $query->where('company_id', $companyId);
-            })->get();
+            });
+        
         } else {
             return redirect()->back()->with('error', 'User not authorized to view tasks.');
         }
-  
+
+        $suppliers = $suppliers->whereHas('companies', function ($query){  
+            $query->where('is_active', true);
+        })->get();
+
         $taskCount = $tasks->count();
         $tasks = $tasks->orderBy($sortBy, $sortOrder)
             ->orderBy('id', $sortOrder)

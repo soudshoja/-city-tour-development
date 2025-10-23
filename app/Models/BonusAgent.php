ab609abb3156204f5ed0a953f16ad213ef9228c9
maizatulmokhtar <maizatulmokhtar85@gmail.com>
2025-10-23 15:34:38 +0800

    feat: pv bonus payment

---
+++ app/Models/BonusAgent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BonusAgent extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'transaction_id',
+        'agent_id',
+        'amount',
+        'created_by',
+        'company_id',
+        'branch_id',
+        'description',
+    ];
+
+    protected $casts = [
+        'transaction_id' => 'integer',
+        'agent_id' => 'integer',
+        'amount' => 'decimal:2',
+        'created_by' => 'integer',
+    ];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transaction_id');
+    }
+
+    public function agent()
+    {
+        return $this->belongsTo(Agent::class, 'agent_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}

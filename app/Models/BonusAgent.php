03cfeb6cc827c77640c6552d386473d72969e625
maizatulmokhtar <maizatulmokhtar85@gmail.com>
2025-10-23 18:13:06 +0800

    feat: bonus at agent crm

--- app/Models/BonusAgent.php
+++ app/Models/BonusAgent.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BonusAgent extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'transaction_id',

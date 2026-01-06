fba01cdc1f60ab48bada9d396e6c516cdad10cfa
maizatulmokhtar <maizatulmokhtar85@gmail.com>
2026-01-06 14:37:34 +0800

    feat: enabled task report, npm build, hide confirmed tasks

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -291,6 +291,12 @@
                 class="text-xs justify-center text-center p-3 my-3 bg-white text-gray-600 dark:bg-gray-700 dark:text-white BoxShadow">Daily Sales</a>
             </menuitem>
             @endcan
+            @can('viewTaskReport', 'App\Models\Report')
+            <menuitem>
+            <a href="{{ route('reports.tasks') }}"
+                class="text-xs justify-center text-center p-3 my-3 bg-white text-gray-600 dark:bg-gray-700 dark:text-white BoxShadow">Task Report</a>
+            </menuitem>
+            @endcan
         </menu>
         </menuitem>
 
